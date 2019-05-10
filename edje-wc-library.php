74b3863700616bca5f6cc51d9192b21e6b45e08a
Henner Setyono <henner.renardi@gmail.com>
2019-05-10 14:53:06 +0700

    2.2.1
    - Updated the Plugin info

--- edje-wc-library.php
+++ edje-wc-library.php
@@ -2,9 +2,9 @@
 /*
 Plugin Name: Edje WooCommerce Library
 Description: Simplify WooCommerce complicated features. Designed to work with Timber.
-Plugin URI: http://github.com/hrsetyono/woocommerce-edje
-Author: The Syne Studio
-Author URI: http://thesyne.com/
+Plugin URI: http://github.com/hrsetyono/edje-wc-library
+Author: Pixel Studio
+Author URI: https://pixelstudio.id/
 Version: 2.2.0
 */
 
