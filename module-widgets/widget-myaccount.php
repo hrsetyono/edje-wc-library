5d6d5ac1fc2a59918a80c965c67d6b24f6206caf
Henner S <henner.renardi@gmail.com>
2022-09-05 16:53:15 +0700

    4.3.1
    - [Widget] Added a new filter `hwc_widget_myaccount_args` to change the argument for My Account widget

--- module-widgets/widget-myaccount.php
+++ module-widgets/widget-myaccount.php
@@ -16,12 +16,12 @@ class HWC_Widget_MyAccount extends HWC_Widget {
     $label_logged_out = get_field('label_logged_out', $widget_id);
     $label_logged_in = get_field('label_logged_in', $widget_id);
 
-    $data = [
+    $data = apply_filters('hwc_widget_myaccount_args', [
       'label_logged_out' => $label_logged_out ? $label_logged_out : 'Login',
       'label_logged_in' => $label_logged_in ? $label_logged_in : 'View Profile',
       'icon' => '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width="50" height="50"><path d="M224 256c70.7 0 128-57.3 128-128S294.7 0 224 0 96 57.3 96 128s57.3 128 128 128zm89.6 32h-16.7c-22.2 10.2-46.9 16-72.9 16s-50.6-5.8-72.9-16h-16.7C60.2 288 0 348.2 0 422.4V464c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48v-41.6c0-74.2-60.2-134.4-134.4-134.4z"/></svg>',
-    ];
-
+    ]);
+  
     $custom_render = apply_filters('hwc_widget_myaccount', '', $data);
 
     echo $args['before_widget'];
@@ -43,14 +43,16 @@ class HWC_Widget_MyAccount extends HWC_Widget {
     >
       <?= $icon ?>
 
-      <?php if (is_user_logged_in()): ?>
+      <?php if (is_user_logged_in() && $label_logged_in): ?>
         <span>
           <?= $label_logged_in ?>
         </span>
-      <?php else: ?>
+
+      <?php elseif ($label_logged_out): ?>
         <span>
           <?= $label_logged_out ?>
         </span>
+
       <?php endif; ?>
     </a>
 
